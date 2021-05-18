27ef5e13964ba0f2dae32ba4ec67c94a86088d98
meva <ebrahim.v.aghaie1391@gmail.com>
2021-05-18 16:39:18 +0430

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 import './App.css';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <>
+      <Particles 
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle", 
+              stroke: {
+                width: 6,
+                color: "#fff"
+              }
+            }
+          }
+        }}
+      />
       <Navbar />
+      <Header />
     </>
   );
 }
